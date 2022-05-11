36122fdfdc7f86cec206868203b88227189e9dc7
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-11 11:23:04 +0300

    [INIT] db

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, Dispatcher
 from aiogram import types
 
+from src.__init__ import DB
 from config import Config
 
 bot = Bot(token=Config.TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot)
+db = DB
