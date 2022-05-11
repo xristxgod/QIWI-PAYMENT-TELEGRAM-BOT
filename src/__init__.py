481819e7c151e386d3ad480624c6131b3ab6baec
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-11 11:54:46 +0300

    [ADD] new method in db

--- src/__init__.py
+++ src/__init__.py
@@ -12,7 +12,13 @@ class DB:
     table = user_model
         id: Integer Primary Key
         username: String(256) DEFAULT = NULL
-        balance: decimal NOT NULL UNIQUE = TRUE DEFAULT = 0.0
+        balance: Decimal NOT NULL DEFAULT = 0.0
+    <<<--------------------------------------------------->>>
+    table = check_model
+        id: Integer Primary Key
+        bill_id: Integer DEFAULT = NULL
+        amount: Decimal NOT NULL DEFAULT = 0.0
+        user_id: ForeignKey(user_model)
     <<<--------------------------------------------------->>>
     """
     @staticmethod
@@ -35,16 +41,49 @@ class DB:
 
     @staticmethod
     async def user_exists(user_id: TGChatID) -> bool:
-        return bool(len(await DB._select_method("SELECT * FROM user_model WHERE id = 1$;", (user_id,))))
+        return bool(len(await DB._select_method(
+            "SELECT * FROM user_model WHERE id = 1$;",
+            (user_id,)
+        )))
 
     @staticmethod
     async def add_user(user_id: TGChatID, username: TGUsername) -> bool:
-        return await DB._insert_method("INSERT INTO user_model (id, username) VALUES ($1, $2);", (user_id, username))
+        return await DB._insert_method(
+            "INSERT INTO user_model (id, username) VALUES ($1, $2);",
+            (user_id, username)
+        )
 
     @staticmethod
     async def get_user_balance(user_id: TGChatID) -> decimal.Decimal:
-        return await DB._select_method("SELECT balance FROM user_model WHERE id = 1$;", (user_id,))
+        return await DB._select_method(
+            "SELECT balance FROM user_model WHERE id = 1$;",
+            (user_id,)
+        )
 
     @staticmethod
     async def set_user_balance(user_id: TGChatID, balance: decimal.Decimal) -> bool:
-        return await DB._insert_method("UPDATE user_model SET balance = 1$ WHERE id = $2", (balance, balance))
+        return await DB._insert_method(
+            "UPDATE user_model SET balance = 1$ WHERE id = $2;",
+            (balance, user_id)
+        )
+
+    @staticmethod
+    async def add_check(user_id: TGChatID, amount: decimal.Decimal, bill_id: int) -> bool:
+        return await DB._insert_method(
+            "INSERT INTO check_model (amount, bill_id, user_id) VALUES (1$, 2$, 3$);",
+            (amount, bill_id, user_id)
+        )
+
+    @staticmethod
+    async def get_check(user_id: TGChatID, bill_id: int):
+        return await DB._select_method(
+            "SELECT bill_id, amount, user_id FROM check_model WHERE user_id = 1$ AND bill_id = 2$;",
+            (user_id, bill_id)
+        )
+
+    @staticmethod
+    async def delete_check(user_id: TGChatID, bill_id: int):
+        return await DB._insert_method(
+            "DELETE FROM check_model WHERE user_id = 1$ AND bill_id = 2$;",
+            (user_id, bill_id)
+        )
